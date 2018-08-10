70b066459d1f08c7b1b7aaacf4350e069b4eeb5c
Steven Crouse <sdcrouse91@gmail.com>
2018-08-10 07:08:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,15 @@
 def oxford_comma(array)
+  # Note that I'm not expecting an empty array.
+  
   case array.size 
+  
   when 1 
     array[0]
   when 2 
     array.join(" and ")
   else
     last = array.pop
-    array.join(", ") + "and " + last
+    array.join(", ") + " and " + last
   end
+  
 end
