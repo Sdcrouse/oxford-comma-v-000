ffd7e521ab464f8e451eb0b1cea14d27dae60609
Steven Crouse <sdcrouse91@gmail.com>
2018-08-10 07:19:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,8 +9,8 @@ def oxford_comma(array)
   when 2 
     array.join(" and ")
   else
-    # last = array.pop # This is destructive!
-    last = array.slice(-1) # This is non-destructive.
+    last = array.pop # This is destructive!
+    # last = array.slice(-1) # This would cause the last element to be repeated.
     array.join(", ") + ", and " + last
   end
   
